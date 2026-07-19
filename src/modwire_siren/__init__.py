6f60ddb367dc6db165e7916920767096721f987f
Tomasz Szpak <tomszp@gmail.com>
2026-07-19 20:09:27 +0200

    add

--- src/modwire_siren/__init__.py
+++ src/modwire_siren/__init__.py
@@ -1,11 +1,7 @@
-from .builder import SirenBuilderService
-from .contracts import SirenApi, SirenContext, SirenField, SirenOperation, SirenResource, SirenRoot, SirenRoute
-from .engine import SirenEngine
+from .contracts import SirenContext
 from .service import SirenApiService
 
 __all__ = [
-    "SirenApi",
     "SirenApiService",
-    "SirenBuilderService",
     "SirenContext",
 ]
